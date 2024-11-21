4dad225c8627aa54052a2cea344510444f5be714
Ram Kumar Dhimal <ramkumardhimal2058@gmail.com>
2024-11-22 01:37:20 +1100

    something more

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 console.log("It is working");
 const apiUrl = "https://api.currencyapi.com/v3/latest?apikey=cur_live_FvIHNNFlFovzJw4s0GPJEHNALPi4TxCXFGcZXVHZ";
+let data = [];
+const convert = document.getElementById("button");
+
+convert.addEventListener("click", () => {
+    console.log("The button is clicked");
+
+})
+
