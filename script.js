e001c99512021f55c92291c69129e97cafb330cd
Ram Kumar Dhimal <ramkumardhimal2058@gmail.com>
2024-11-24 12:34:39 +1100

    loading and the output table

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ console.log("It is working");
 const apiUrl = "https://api.currencyapi.com/v3/latest?apikey=cur_live_FvIHNNFlFovzJw4s0GPJEHNALPi4TxCXFGcZXVHZ";
 let dataList = [];
 const loading = document.getElementById("loading");
+const output = document.getElementById("output");
 const tBody = document.getElementById("output-table-body")
 const convert = document.getElementById("button");
 
@@ -25,6 +26,7 @@ const dataFromApi = async () => {
     }
 
     loading.style.display = "none";
+    output.style.display = "block";
 }
 dataFromApi();
 
